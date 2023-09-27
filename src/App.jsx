33368227da958d2eb11cf288de4f030a72f028bc
Fathan Mubarok <50894040+falonez@users.noreply.github.com>
2023-09-28 04:09:29 +0700

    feat(quiz): slicing quiz form

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-full bg-[#2F2F2F] fixed flex flex-col justify-between items-center py-10">
+        <div className="p-6 mx-10 bg-blueku rounded-xl flex justify-center self-start">
+          <h2 className="text-white text-xl">Time : 09.50</h2>
+        </div>
+        <div className="bg-blueku h-[40vh] w-full flex justify-center items-center text-[3rem] p-20 text-white">
+          <h2>An interface design application that runs in the browser with team-based collaborative design projects</h2>
+        </div>
+        <div className="flex w-full justify-around">
+          <div className="bg-yellowku text-white w-1/4 mx-4 text-center text-[2rem] h-[20vh] font-semibold shadow-md flex justify-center items-center"><h1>FIGMA</h1></div>
+          <div className="bg-greyku w-1/4 mx-4 text-center text-[2rem] h-[20vh] font-semibold shadow-md flex justify-center items-center">ADOBE XD</div>
+          <div className="bg-greyku w-1/4 mx-4 text-center text-[2rem] h-[20vh] font-semibold shadow-md flex justify-center items-center">INVISION</div>
+          <div className="bg-greyku w-1/4 mx-4 text-center text-[2rem] h-[20vh] font-semibold shadow-md flex justify-center items-center">SKETCH</div>
+        </div>
+        <div className="text-2xl text-white">10 dari 23</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+      
     </>
   )
 }
