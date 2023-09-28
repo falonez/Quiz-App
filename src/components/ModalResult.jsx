55a1aae98001af1dd43ec0508eeefd82da2b4b3b
Fathan Mubarok <50894040+falonez@users.noreply.github.com>
2023-09-28 07:25:13 +0700

    feat(quiz): result quiz

--- src/components/ModalResult.jsx
+++ src/components/ModalResult.jsx
@@ -1,32 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ModalResult = () => {
+const ModalResult = ({correctAnswer, wrongAnswer, totalAnswer}) => {
+    const [detail, setDetail] = useState(false)
+    // menghitung Hasil score 
+    const countScore = () => {
+        const score = (correctAnswer / totalAnswer) * 100
+        return Math.floor(score)
+    }
+    
+    const handleBack = () => {
+        // Menghapus data di localstorage
+        localStorage.removeItem('isLogin')
+        localStorage.removeItem('question')
+        window.location.reload()
+    }
+
+    const handleDetail = () => {
+        // Detail Score
+        setDetail(true)
+    }
   return (
     <div>
-      <div className='fixed bg-black/60 w-full h-full flex justify-center items-center'>
+      <div className='fixed bg-black/60 w-full h-full flex justify-center items-center z-20'>
         <div className='w-1/2 h-2/3 bg-blueku rounded-2xl flex justify-center flex-col items-center'>
           {/* Skor  */}
-          <h1 className='text-[5rem] font-semibold text-white'>Your Score</h1>
-          <h1 className='text-[15rem] font-bold text-yellowku'>100</h1>
-          <div className='flex flex-row gap-10'> 
-          <button className='bg-yellowku rounded-md p-3 text-white font-semibold'>Detail Here</button>
-          <button className='bg-greyku rounded-md p-3 text-white font-semibold'>Back Here</button>
-          </div>
-          {/* Detail
-          <div className='w-full h-2/3 bg-blueku rounded-2xl flex justify-between flex-col items-center'>
+          {!detail && 
+          <>
+            <h1 className='text-[5rem] font-semibold text-white'>Your Score</h1>
+            <h1 className='text-[15rem] font-bold text-yellowku'>{countScore()}</h1>
+            <div className='flex flex-row gap-10'> 
+            <button className='bg-yellowku rounded-md p-3 text-white font-semibold' onClick={handleDetail}>Detail Here</button>
+            <button className='bg-greyku rounded-md p-3 text-white font-semibold' onClick={handleBack}>Back Here</button>
+            </div>
+          </>
+            }
+          {/* Detail */}
+          {detail && <div className='w-full h-2/3 bg-blueku rounded-2xl flex justify-between flex-col items-center'>
             <h1 className='text-[3rem] font-semibold text-white'>Detail Score</h1>
             <div className='flex flex-row justify-center items-center w-[50%]'>
               <div className='grid grid-cols-2 w-full h-full text-center gap-5'>
                 <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>Jumlah Benar</div>
-                <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>2 Soal</div>
+                <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>{correctAnswer} Soal</div>
                 <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>Jumlah Salah</div>
-                <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>2 Soal</div>
+                <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>{wrongAnswer} Soal</div>
                 <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>Jumlah jawab</div>
-                <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>2 Soal</div>
+                <div className='w-full h-full justify-between text-[2rem] font-medium text-yellowku'>{totalAnswer} Soal</div>
               </div>
             </div>
-              <button className='bg-yellowku rounded-md p-3 text-white font-semibold'>Back Here</button>
-          </div> */}
+              <button className='bg-yellowku rounded-md p-3 text-white font-semibold' onClick={handleBack}>Back Here</button>
+          </div>}
         </div>
       </div>
     </div>
